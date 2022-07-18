eb948cb9d49bc492925c005b45dac17a0e757eba
Soboleva Natalia <nsoboleva1979@rambler.ru>
2022-07-19 00:51:17 +0300

    initial comit

---
+++ webpack.prod.js
@@ -0,0 +1,10 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  optimization: {
+    minimize: true,
+  },
+});
